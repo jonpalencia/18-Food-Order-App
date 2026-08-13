a6d953004c4428291aa2be4aaff2fa4200604a1d
jonpalencia <jonpalencia20@gmail.com>
2026-08-14 05:15:12 +0800

    Refactor App.jsx to import and provide the CartContext

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
+import CartContextProvider from './store/cartContext';
 import Header from './components/Header';
 import Meals from './components/Meals';
 
 function App() {
   return (
-    <>
+    <CartContextProvider>
       <Header />
       <Meals />
-    </>
+    </CartContextProvider>
   );
 }
 
