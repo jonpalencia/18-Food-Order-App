a755eb4429a6fea366fa48f7cf3853a4b52c5e56
jonpalencia <jonpalencia20@gmail.com>
2026-08-15 05:31:32 +0800

    refactor - add reducer function and dispatch function tests.

--- src/store/cartContext.jsx
+++ src/store/cartContext.jsx
@@ -3,31 +3,96 @@ import { createContext, useReducer, useState } from 'react';
 export const CartContext = createContext({
   items: [],
   addItem: item => {},
-  updateItem: id => {},
+  removeItem: id => {},
 });
 
 // Reducer function
 function cartReducer(state, action) {
   if (action.type === 'ADD_ITEM') {
-    // ... Add logic here.
+    // ... Add to cart feature
+    const existingCartItemIndex = state.items.findIndex(
+      item => item.id === action.payload.id,
+    );
+    const updatedItems = [...state.items];
+
+    if (existingCartItemIndex > -1) {
+      const existingItem = state.items[existingCartItemIndex];
+      const updatedItem = {
+        ...existingItem,
+        quantity: existingItem.quantity + 1,
+      };
+      updatedItems[existingCartItemIndex] = updatedItem;
+    } else {
+      updatedItems.push({ ...action.payload, quantity: 1 });
+    }
+
+    return {
+      ...state,
+      items: updatedItems,
+    };
   }
 
-  if (action.type === 'UPDATE_ITEM') {
-    // ... Add logic here.
+  if (action.type === 'REMOVE_ITEM') {
+    // ... Update cart feature
+
+    const existingCartItemIndex = state.items.findIndex(
+      item => item.id === action.payload,
+    );
+
+    const existingCartItem = state.items[existingCartItemIndex];
+
+    // Guard clause if no quantity yet
+    if (!existingCartItem?.quantity) {
+      return { ...state };
+    }
+
+    const updatedItems = [...state.items];
+
+    if (existingCartItem.quantity === 1) {
+      updatedItems.splice(existingCartItemIndex, 1);
+    } else {
+      const updatedItem = {
+        ...existingCartItem,
+        quantity: existingCartItem.quantity - 1,
+      };
+
+      updatedItems[existingCartItemIndex] = updatedItem;
+    }
+
+    return {
+      ...state,
+      items: updatedItems,
+    };
   }
 
-  return {
-    ...state,
-  };
+  return state;
 }
 
 export default function CartContextProvider({ children }) {
   const [cartState, dispatch] = useReducer(cartReducer, { items: [] });
 
+  const addItem = function (item) {
+    // ... Add to cart feature
+    dispatch({
+      type: 'ADD_ITEM',
+      payload: item,
+    });
+  };
+
+  const removeItem = function (id) {
+    // ... Remove item feature
+    dispatch({
+      type: 'REMOVE_ITEM',
+      payload: id,
+    });
+  };
+
+  console.log(cartState);
+
   const cartCtx = {
     items: cartState.items,
-    addItem: item => {},
-    updateItem: id => {},
+    addItem,
+    removeItem,
   };
 
   return (
