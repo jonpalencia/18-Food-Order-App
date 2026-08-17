00ee5c18fc065b6a6f03a6cf6632ddc71f7dec6a
jonpalencia <jonpalencia20@gmail.com>
2026-08-18 03:38:55 +0800

    Outsource, create new dir folder for Cart only components.

---
+++ src/components/meals/MealItem.jsx
@@ -0,0 +1,34 @@
+import { use, useCallback, useContext } from 'react';
+import { CartContext } from '../../store/cartContext';
+import Button from '../UI/Button';
+import { MEALS_URL } from '../../utils/config';
+import { currencyFormatter } from '../../utils/utils';
+
+export default function MealItem({ meal }) {
+  const { id, name, image, description, price } = meal;
+  const { items, addItem, removeItem } = useContext(CartContext);
+
+  const handleAddToCart = function (e) {
+    addItem(meal);
+  };
+
+  const handleRemoveItem = function () {
+    removeItem(id);
+  };
+
+  return (
+    <li className="meal-item" key={id}>
+      <article>
+        <img src={`${MEALS_URL}/${image}`} alt={name} />
+        <div>
+          <h3>{name}</h3>
+          <p className="meal-item-price">{currencyFormatter.format(price)}</p>
+          <p className="meal-item-description">{description}</p>
+        </div>
+        <p className="meal-item-actions">
+          <Button onClick={handleAddToCart}>Add to Cart</Button>
+        </p>
+      </article>
+    </li>
+  );
+}
