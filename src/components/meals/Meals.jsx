c3bb986cd472bc84346d427c7f732596020cc5ba
jonpalencia <jonpalencia20@gmail.com>
2026-08-26 06:59:26 +0800

    Refactor Meals.jsx component to use custome component useHttp

--- src/components/meals/Meals.jsx
+++ src/components/meals/Meals.jsx
@@ -1,22 +1,15 @@
 import { useEffect, useState } from 'react';
-import { MEALS_URL } from '../../utils/config';
 import MealItem from './MealItem';
+import { MEALS_URL } from '../../utils/config';
+import { defaultObj, sendHttpRequest } from '../../utils/utils';
+import useHttp from '../../hooks/useHttp';
 
 export default function Meals() {
-  const [mealsState, setMealsState] = useState([]);
-
-  useEffect(() => {
-    (async function () {
-      try {
-        const res = await fetch(`${MEALS_URL}/meals`);
-        if (!res.ok) throw new Error(`🚫 ${res.status} - Please try again...`);
-        const resData = await res.json();
-        setMealsState(resData);
-      } catch (err) {
-        console.error(err);
-      }
-    })();
-  }, []);
+  const { data: mealsState } = useHttp(
+    `${MEALS_URL}/meals`,
+    defaultObj.method,
+    defaultObj.initData,
+  );
 
   return (
     <ul id="meals">
