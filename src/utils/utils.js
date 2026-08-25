5c1125f9e06dab0f9c3d81d48bdfc215fe8b4fa5
jonpalencia <jonpalencia20@gmail.com>
2026-08-26 06:56:19 +0800

    Refactor - add default objects for req methods.

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -1,4 +1,18 @@
+export const defaultObj = {
+  method: {},
+  initData: [],
+};
+
 export const currencyFormatter = new Intl.NumberFormat(navigator.language, {
   style: 'currency',
   currency: 'PHP',
 });
+
+export const sendHttpRequest = async function (url, config = {}) {
+  const response = await fetch(url, config);
+  const resData = await response.json();
+  if (!response.ok) {
+    throw new Error(`${response.status} - Request failed, please try again...`);
+  }
+  return resData;
+};
